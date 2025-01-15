c800045e7ae1e413bd16b28eee99aa9908fd3ff2
Алексей Зенков <zenkov.m700@mail.ru>
2025-01-15 16:57:33 +0300

    cost + define

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-const name = 'value';
-define('s', 'varlue');
-echo name, s;
+//const name = 'value';
+//define('s', 'varlue');
+//echo name, s;
+
+
+$paid = 'PAID';
+define('STATUS_' . $paid, $paid);
+
+echo STATUS_PAID;
